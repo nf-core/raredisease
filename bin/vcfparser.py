66678a882ab52827f9778c3c7d9dcfef385d9594
Ramprasad Neethiraj <20065894+ramprasadn@users.noreply.github.com>
2022-09-13 13:25:12 +0200

    review suggestions

--- bin/vcfparser.py
+++ bin/vcfparser.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 import argparse
 import sys
-from collections import Counter
 from pathlib import Path
+from typing import Tuple, TextIO
 
 
-def parse_vep_csq_transcripts(transcripts, allele_ind, csq_ind, hgnc_ind, var_csq):
+def parse_vep_csq_transcripts(
+    transcripts: list, allele_ind: int, csq_ind: int, hgnc_ind: int, var_csq: list
+) -> Tuple[list, list, list, list]:
     """
     Parse conseqences for each transcript and return HGNC IDs, alleles, and their severity rank
     based on the term's ranking in the ensembl consequences list.
@@ -28,7 +30,6 @@ def parse_vep_csq_transcripts(transcripts, allele_ind, csq_ind, hgnc_ind, var_cs
     hgnc_ids = []
     severity = []
     alleles = []
-    mscsq_anno = []
     for transcript in transcripts:
         vep_fields = transcript.strip().split("|")
         csq = vep_fields[csq_ind].split("&")[0]
@@ -41,7 +42,9 @@ def parse_vep_csq_transcripts(transcripts, allele_ind, csq_ind, hgnc_ind, var_cs
     return hgnc_ids, alleles, consequences, severity
 
 
-def construct_most_severe_consequence_info(line, allele_ind, csq_ind, hgnc_ind, var_csq):
+def construct_most_severe_consequence_info(
+    line: str, allele_ind: int, csq_ind: int, hgnc_ind: int, var_csq: list
+) -> list:
     """
     Parse conseqences for each transcript and return HGNC IDs, alleles, and their severity rank
     based on the term's ranking in the ensembl consequences list.
@@ -74,11 +77,12 @@ def construct_most_severe_consequence_info(line, allele_ind, csq_ind, hgnc_ind, 
             most_severe_csq = consequences_sub[severity_sub.index(min(severity_sub))]
             most_severe_allele = alleles_sub[severity_sub.index(min(severity_sub))]
             mscsq_anno.append(gene_id + ":" + most_severe_allele + "|" + most_severe_csq)
-    columns[7] += ";most_severe_consequence=" + ",".join(mscsq_anno)
+    if mscsq_anno:
+        columns[7] += ";most_severe_consequence=" + ",".join(mscsq_anno)
     return columns
 
 
-def find_indices(list_to_check, item_to_find):
+def find_indices(list_to_check: list, item_to_find: str) -> list:
     """
     Get indices of an element in a list
 
@@ -96,7 +100,7 @@ def find_indices(list_to_check, item_to_find):
     return indices
 
 
-def parse_vep_csq_schema(line):
+def parse_vep_csq_schema(line: str) -> Tuple[int, int, int]:
     """
     Get indices of allele, consequence, and hgnc id in the annotation
 
@@ -116,20 +120,16 @@ def parse_vep_csq_schema(line):
     return allele_ind, csq_ind, hgnc_ind
 
 
-def write_csq_annotated_vcf(file_in, file_out, var_csq):
+def write_csq_annotated_vcf(file_in: TextIO, file_out: TextIO, var_csq: list):
     """Add most severe consequence field to record, and write the record to a vcf file"""
-    counter = 0
     for line in file_in:
         if line.startswith("#"):
+            file_out.write(line)
             if line.startswith("##INFO=<ID=CSQ"):
-                counter = 1
                 allele_ind, csq_ind, hgnc_ind = parse_vep_csq_schema(line)
-            elif not line.startswith("##INFO") and counter == 1:
                 file_out.write(
                     '##INFO=<ID=most_severe_consequence,Number=.,Type=String,Description="Most severe genomic consequence.">\n'
                 )
-                counter = 0
-            file_out.write(line)
         else:
             mscsq = construct_most_severe_consequence_info(line, allele_ind, csq_ind, hgnc_ind, var_csq)
             file_out.write("\t".join(mscsq) + "\n")
@@ -139,22 +139,22 @@ def parse_args(argv=None):
     """Define and immediately parse command line arguments."""
     parser = argparse.ArgumentParser(
         description="Annotate vcf with the most severe consequence field.",
-        epilog="Example: python vcfparser.py vep.vcf vep.most_severe_csq.vcf",
+        epilog="Example: python vcfparser.py --file_in vep.vcf --file_out vep.most_severe_csq.vcf --variant_csq variant_consequence.txt",
     )
     parser.add_argument(
-        "file_in",
+        "--file_in",
         metavar="FILE_IN",
         type=Path,
         help="Vcf file annotated with vep.",
     )
     parser.add_argument(
-        "file_out",
+        "--file_out",
         metavar="FILE_OUT",
         type=Path,
         help="Vcf with most_severe_consequence annotations added to it.",
     )
     parser.add_argument(
-        "variant_csq",
+        "--variant_csq",
         metavar="VARIANT_CSQ",
         type=Path,
         help="Variant consequences ranked by severity",
