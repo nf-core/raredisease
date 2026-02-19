546639adf538527ee462aba8b82b08a24731bbc7
Ramprasad Neethiraj <20065894+ramprasadn@users.noreply.github.com>
2026-02-19 19:35:20 +0100

    remove logs

--- test.sh
+++
@@ -1,2 +0,0 @@
-nf-test test subworkflows/local/ --profile test,singularity --verbose --debug --update-snapshot --clean-snapshot >wflog 2>&1
-nf-test test tests --profile test,singularity --verbose --debug --update-snapshot --clean-snapshot >>wflog 2>&1
